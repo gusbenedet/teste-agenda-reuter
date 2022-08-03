2f274581354bde440c2bf00e9ec207ee72d6b4bb
Rukah <pedrolucas.cremonini@gmail.com>
2022-08-03 20:10:25 -0300

    arrumando coisa

--- jsagenda.js
+++ jsagenda.js
@@ -4,13 +4,46 @@ var constlogin = [
     'teste@3'
 ]
 
-function logar(){
-    js_login = document.getElementById('login').value 
-    valid = 0
+const constsenha = {
+    'teste@1'(y) {
+        if (y == "senha") {
+            valid = 1
+        }
+    },
+}
 
 
-    if (-1  !== constlogin.indexOf(js_login)){
-        window.location.replace("http://stackoverflow.com");
-        
+
+
+function registrar() {
+    senhanova = prompt("insira")
+
+    senhanova = senhanova.toString()
+
+    var contanova = function (y) {
+        if (y == senhanova) {
+            alert("doidera")
+            valid = 1
+        }
+    }
+    constsenha['teste@2'] = contanova(js_senha)
+}
+
+function logar() {
+    js_login = document.getElementById('login').value
+    js_senha = document.getElementById('senha').value
+    const teste = constsenha[js_login]
+    teste(js_senha)
+    if (-1 !== constlogin.indexOf(js_login)) {
+        switch (valid) {
+            case 1:
+                alert("deu")
+                break;
+
+            default:
+                alert("senha incorreta inserida")
+                break;
+        }
+
     }
 }
